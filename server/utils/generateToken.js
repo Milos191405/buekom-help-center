0eb29b1d38b224033177780d3024817a14401578
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-12-04 10:51:32 +0100

    fixing token error

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,24 +1,28 @@
-import jwt from "jsonwebtoken"; 
+import jwt from "jsonwebtoken";
+
 
 export const generateTokenAndSendCookie = (res, userId, role) => {
   try {
+   
     const token = jwt.sign(
-      { id: userId, role }, 
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
+      { id: userId, role },
+      process.env.JWT_SECRET, 
+      { expiresIn: "1h" } 
     );
 
-    // Send token in the cookie
+    //
     res.cookie("jwt-buekom", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 3600000, // 1 hour
+      httpOnly: true, 
+      secure: process.env.NODE_ENV === "production", 
+      sameSite: "Strict", 
+      maxAge: 3600000, 
     });
 
+    
     return token;
   } catch (error) {
     console.error("Error generating token:", error.message);
+    
     throw new Error("Error generating token");
   }
 };
